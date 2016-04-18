42ec2a85ef30c42bf63496c700eff08a9adcf17d
Fabian Vogelsteller <fabian@frozeman.de>
2016-04-18 15:04:51 +0200

    published version 0.3.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "frozeman:build-client",
     summary: "Placeholder package for meteor-build-client (npm). Do not install!",
-    version: "0.2.2",
+    version: "0.3.0",
     git: "https://github.com/frozeman/meteor-build-client"
 });
 
